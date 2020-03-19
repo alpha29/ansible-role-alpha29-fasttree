d7381f95ec2cac41a89bc54f07426bcc15cf16c5
vanguard737 <vanguard737@gmail.com>
2020-03-19 05:44:54 -0500

    Test accommodates FastTreeDblMP's non-standard exit status.

--- molecule/default/tests/test_fasttree.py
+++ molecule/default/tests/test_fasttree.py
@@ -4,5 +4,6 @@ def test_fasttree(host):
     """
     cmd = "FastTreeDblMP"
     cmd_result = host.run(cmd)
-    assert cmd_result.rc == 0, "'{}' returned status {}.".format(cmd, cmd_result.rc)
+    # FastTreeDblMP returns a non-standard exit status...?
+    assert cmd_result.rc == 1, "'{}' returned status {}.".format(cmd, cmd_result.rc)
     assert "FastTree Version 2.1.10 Double precision" in cmd_result.stderr, f"'{cmd}' returned stdout '{cmd_result.stdout}', stderr '{cmd_result.stderr}'"
