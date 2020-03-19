df8888da615672ced494dcbe6ff049e9ccc2b55f
vanguard737 <vanguard737@gmail.com>
2020-03-19 05:29:47 -0500

    Fixes version in test.

--- molecule/default/tests/test_fasttree.py
+++ molecule/default/tests/test_fasttree.py
@@ -5,4 +5,4 @@ def test_fasttree(host):
     cmd = "FastTreeDblMP"
     cmd_result = host.run(cmd)
     assert cmd_result.rc == 0, "'{}' returned status {}.".format(cmd, cmd_result.rc)
-    assert "FastTree 1.2.3.4.5" in cmd_result.stdout, f"'{cmd}' returned stdout '{cmd_result.stdout}', stderr '{cmd_result.stderr}'"
+    assert "FastTree Version 2.1.10 Double precision" in cmd_result.stderr, f"'{cmd}' returned stdout '{cmd_result.stdout}', stderr '{cmd_result.stderr}'"
